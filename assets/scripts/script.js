4d271bb62f33f2670e772f145326c87e6852583d
OldWhaler <76100109+OldWhaler@users.noreply.github.com>
2022-07-06 18:10:23 +0300

    feat: add solution to homework-14

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+const questionWrappers = document.querySelectorAll('.faq__question-wrapper');
+
+questionWrappers.forEach(questionWrapper => questionWrapper.addEventListener('click', questionWrapperClickHandeler))
+
+function questionWrapperClickHandeler() {
+  console.log(this)
+  this.querySelector('.faq__arrow-button').classList.toggle('faq__arrow-button_modified');
+  this.querySelector('.faq__answer').classList.toggle('visually-hidden');
+}
